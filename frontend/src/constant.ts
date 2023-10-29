dad053215d165356417bcb4144158fead9d2f0c1
phucnt <nguyentrongphucnn205@gmail.com>
2023-10-29 11:02:24 +0700

    Update constant file

--- frontend/src/constant.ts
+++ frontend/src/constant.ts
@@ -1,9 +1,13 @@
 import { getToken } from './helpers'
 export const AVATAR_API = 'https://ui-avatars.com/api'
+// Config port
 export const API = 'http://localhost:44388/api'
-export const Auth_API = 'http://localhost:7111/api/Auth'
-export const Episode_API = 'http://localhost:7113/api/Episodes'
-export const Movie_API = 'http://localhost:7113/api/Movies'
+export const Auth_API = 'https://localhost:44388/api/Auth'
+export const Episode_API = 'http://localhost:44384/api/Episodes'
+export const Movie_API = 'http://localhost:44384/api/Movies'
+export const Category_API = 'https://localhost:44386/api/Categories'
+
+// Config token
 export const AUTH_TOKEN = 'authToken'
 export const REFRESH_TOKEN = 'refreshToken'
 export const BEARER = 'Bearer'
